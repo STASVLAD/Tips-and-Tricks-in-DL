5fc0290f1ad5192c5a8f35a05ed9dd0eb4c12e21
Avkostanov <avkostanov@gmail.com>
2021-03-12 09:56:20 +0300

    add app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 import streamlit as st
+
+st.title('Tips And Tricks 2021 Traffic Lights Detection Project')
+st.write('Made by Nikita Paschenko, and Alexey Kostanov')
+
+st.video(st.file_uploader('Upload your video', ['mp4'], False))
