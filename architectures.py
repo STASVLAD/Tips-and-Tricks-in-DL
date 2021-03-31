9989f64d66a6f177ee0c8cda94141218cf630774
avKostanov <avkostanov@gmail.com>
2021-03-31 17:33:20 +0300

    Delete architectures.py

--- architectures.py
+++
@@ -1,5 +0,0 @@
-import torch
-from torchvision.models.segmentation import deeplabv3_resnet50 as resnet50
-from torchvision.models.detection import fasterrcnn_resnet50_fpn as fasterrcnn
-
-FasterRCNN = fasterrcnn()
